3c0a60bb75bed4acf06d7c59912d15d798028e15
edizrgn <ediz25e@gmail.com>
2026-03-20 15:27:45 +0300

    footer düzenlemeleri yapıldı

--- includes/footer.php
+++ includes/footer.php
@@ -26,15 +26,16 @@
             <div class="col-6 col-lg-3">
                 <h3 class="footer-title">Sosyal</h3>
                 <ul class="footer-list">
-                    <li><a href="#">Twitter</a></li>
+                    <li><a href="https://github.com/edizrgn/notbul">GitHub</a></li>
                     <li><a href="#">Instagram</a></li>
                     <li><a href="#">YouTube</a></li>
-                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">X</a></li>
                 </ul>
             </div>
         </div>
         <div class="footer-bottom mt-4 pt-3 d-flex flex-column flex-md-row justify-content-between gap-2">
             <p class="mb-0">Not Bul | notbul.site</p>
+            <p class="mb-0">Pre-Alpha</p>
             <p class="mb-0">Tüm hakları saklıdır.</p>
         </div>
     </div>
